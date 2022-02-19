4cb6d9c90b86801235be49fbd40adab37244c5af
Cory Barney <cory_barney@msn.com>
2022-02-18 19:56:14 -0800

    Fix up integration tests
    Integration test now will actually use RetroDebugger library.
    Added in binary folder staging to

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,12 @@
 #include <DebuggerApi.h>
 
+#include <fmt/core.h>
+
 int main() {
+
+    std::string cmdPrompt;
+    GetCommandPrompt(&cmdPrompt);
+    fmt::print("Its Here! {}\n", cmdPrompt);
 
     return 0;
 }
