86ac4003bb88d6d0f7e6006c0b86a31c76025d9e
Cory Barney <cory_barney@msn.com>
2023-08-03 17:50:43 -0700

    Cleanup cmake for runtime install component
    Can now install with runtime component to avoid bringing in all items.

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -5,33 +5,27 @@ include(GNUInstallDirs)
 install(
     TARGETS RetroDebugger RetroDebuggerCommon
     EXPORT RetroDebuggerLib
-    ARCHIVE COMPONENT development
-    LIBRARY COMPONENT runtime 
-    FILE_SET HEADERS
-    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/RetroDeb
-    COMPONENT runtime)
+    ARCHIVE COMPONENT runtime
+    LIBRARY COMPONENT runtime
+    RUNTIME COMPONENT runtime
+    FILE_SET HEADERS COMPONENT runtime
+    PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR} COMPONENT runtime)
 # cmake-format: on
 
 if(UNIX)
     install(CODE "execute_process(COMMAND ldconfig)" COMPONENT runtime)
 endif()
 
-message("CMAKE_INSTALL_LIBDIR: ${CMAKE_INSTALL_LIBDIR}")
 install(
     EXPORT RetroDebuggerLib
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/RetroDeb/cmake
     NAMESPACE RetroDeb::
     COMPONENT runtime)
 
-install(FILES "RetroDebuggerConfig.cmake" DESTINATION ${CMAKE_INSTALL_LIBDIR}/RetroDeb/cmake)
-
-# # Runtime
-# # cmake-format: off
-# install(TARGETS RetroDebugger
-#     FILE_SET HEADERS 
-#     PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/RetroDeb
-#     RUNTIME COMPONENT runtime)
-# # cmake-format: on
+install(
+    FILES "RetroDebuggerConfig.cmake"
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/RetroDeb/cmake
+    COMPONENT runtime)
 
 # CPack configuration
 set(CPACK_PACKAGE_VENDOR "YodasYodeler")
